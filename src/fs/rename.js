d712200b862344a04ac948205ca943696093a9e8
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-15 15:39:02 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { rename as renameFile } from 'fs'; 
+
+import {
+    fileURLToPath
+} from 'url';
+import {
+    join,
+    dirname
+} from 'path';
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    renameFile(
+        join(__dirname,'files/wrongFilename.txt'),
+        join(__dirname,'files/properFilename.md'),
+        (err) => {
+        err
+            ? console.log('FS operation failed')
+            : console.log('File renamed successfully!');
+
+    });
 };
 
 await rename();
