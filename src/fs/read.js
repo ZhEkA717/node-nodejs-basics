653fccc7c955f00c430c76e10b8cff01a77d6d2e
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-15 16:07:05 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs';
+
+import {fileURLToPath} from 'url';
+import {join,dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    readFile(join(__dirname, 'files/fileToRead.txt'), 'utf8', (err, data) => {
+        err
+            ? console.log('FS operation failed')
+            : console.log(data);
+    });
 };
 
 await read();
