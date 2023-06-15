db897790d5fb284c530eee9845d9fecf55ab9a76
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-15 11:49:38 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import {
+    writeFile,
+    readFile
+} from 'fs';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const path = join(__dirname, 'files/testFile.txt');
+
 const create = async () => {
-    // Write your code here 
+
+    readFile(path, 'utf8', async (err, data) => {
+
+        data
+            ?
+            console.log('FS operation failed') :
+            writeFile(
+                path,
+                "I am fresh and young",
+                (err) => {
+                    if (err) throw err;
+                    console.log('File was created');
+                });
+    })
 };
 
 await create();
