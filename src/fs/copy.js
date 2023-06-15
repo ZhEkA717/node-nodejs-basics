875539e4ff9843a89b57443514407833ee561661
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-15 15:25:13 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,73 @@
+import {
+    mkdir,
+    readdir,
+    copyFile,
+    stat,
+    access
+} from 'fs';
+
+import {
+    fileURLToPath
+} from 'url';
+import {
+    join,
+    dirname
+} from 'path';
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = dirname(__filename);
+
+const isCopied = () => {
+    return new Promise((resolve) => {
+        access(join(__dirname, 'files-copy'), err => {
+            resolve(err);
+        })
+    });
+}
+
+const createCopyFolder = () => {
+    return new Promise((resolve) => {
+        mkdir(join(__dirname, 'files-copy'),
+            err => {
+                if (err) console.log('FS operation failed');
+                console.log("folder 'files-copy' was created");
+                resolve();
+            });
+    });
+}
+
+const readFolder = () => {
+    return new Promise((resolve) => {
+        readdir(join(__dirname, 'files'),
+            (err, files) => {
+                if (err) throw err;
+                resolve(files);
+            })
+    });
+}
+
 const copy = async () => {
-    // Write your code here 
-};
+    if (await isCopied()) {
+        await createCopyFolder();
+
+        const files = await readFolder();
+
+        files.forEach((file) => {
+            copyFile(
+                join(__dirname, "files", file),
+                join(__dirname, "files-copy", file),
+                (err) => {
+                    if (err) throw err;
+                    console.log(`file "${file}"  was copied`);
+                }
+            )
+        });
+    } else {
+        console.error('FS operation failed');
+    }
+
+}
+
 
 await copy();
