04cff99482e62793c23c14bf3069bce984d711c6
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-16 07:13:47 +0300

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import { createReadStream } from "fs";
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const readableStream = createReadStream(join(__dirname,"files/fileToRead.txt"),"utf-8");
+
+
 const read = async () => {
-    // Write your code here 
+    let data = "";
+    readableStream.on("data", (chunk) => {
+        data += chunk;
+    });
+
+    readableStream.on("end", () => {
+        console.log(data);
+    });
 };
 
 await read();
+
+
+
+
