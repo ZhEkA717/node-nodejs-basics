d6b6850dabf245b9c289143a28b62bb9ad84cace
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-16 07:34:53 +0300

    feat: implement streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import { appendFile } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { stdin } from 'process'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    console.log('Введите текст...')
+
+    stdin.on("data",(data)=>{
+        const myBuffer = Buffer.from(data,"utf-8");
+        const str = myBuffer.toString();
+
+        appendFile(
+            join(__dirname,'files/fileToWrite.txt'),
+            str,
+            err=>{
+                if (err) throw err;
+                console.log("Файл был дополнен..");
+            }
+        )
+    });
 };
 
 await write();
