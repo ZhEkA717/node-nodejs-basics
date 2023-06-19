feefdb8839f410a17e511735ca4a1f4a3ccc727f
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-20 02:43:25 +0300

    feat: start cp block, but not end it

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { spawn } from 'child_process';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    spawn('node', [
+        join(__dirname, 'files/script.js'),
+        ...args
+    ]).stdout.on('data', data => stdout.write(data));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2']);
