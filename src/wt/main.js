5fb752f07f35a3d50d618d874549382b70745928
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-20 02:25:35 +0300

    feat: implement wt block

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = [];
+    
+    const worker = join(__dirname, 'worker.js');
+
+    Array.from(Array(cpus().length).keys())
+        .forEach((item, i) => {
+            workers.push(
+                new Worker(worker, { workerData: 10 + i })
+            );
+        });
+
+    Promise.all(
+        workers.map(worker => new Promise(res => {
+            worker.on('message', (msg) => res({
+                status: 'resolved',
+                data: msg,
+            }));
+
+            worker.on('error', () => res({
+                status: 'error',
+                data: null,
+            }))
+        }))
+    ).then((result) => { console.log(result); });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,11 +1,9 @@
-import { isMainThread, parentPort, workerData } from 'node:worker_threads';
+import { parentPort, workerData } from 'worker_threads';
 
 const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-  if (isMainThread) throw new Error('MainThread');
-  const result = nthFibonacci(workerData);
-  parentPort.postMessage(result);
+  parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
