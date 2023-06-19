ed10eb45305fa095697ec06a3c6fe92fb2a496ae
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-19 23:28:27 +0300

    feat: implement calchash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha1');
+    hash.setEncoding('hex');
+
+    const fd = createReadStream(join(__dirname, 'files/fileToCalculateHashFor.txt'))
+        
+    fd.on('end', () => {
+            hash.end();
+            console.log(hash.read());
+        });
+
+    fd.pipe(hash);
 };
 
 await calculateHash();
